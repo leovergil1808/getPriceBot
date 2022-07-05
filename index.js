5d49332f9589dac3d6d6e69eb5b9e60bd2c52ec6
leovergil1808 <leovergil@gmail.com>
2022-07-05 22:24:51 +0900

    sửa code đổi cấu trúc danh sách coin do thêm chức năng cài thập phân cho giá coin

--- index.js
+++ index.js
@@ -5,28 +5,6 @@ import formData from 'form-data';
 const baseEndPoint = 'http://localhost/autoTradeBot/api'
 // const baseEndPoint = 'https://quandayne.com/biboBot/api'
 
-
-
-let getPrice = async (coins) => {
-    let result = []
-
-    if (coins == undefined || coins.length == 0) return result
-
-    const exchange = new ccxt.huobi();
-    const response = await exchange.fetchTickers(coins)
-    for (const symbol in response) {
-        const item = {
-            coinName: symbol,
-            bid: response[symbol].bid,
-            ask: response[symbol].ask,
-            average: (response[symbol].ask + response[symbol].bid) / 2,
-            spread: (response[symbol].bid && response[symbol].ask) ? response[symbol].ask - response[symbol].bid : undefined
-        }
-        result.push(item)
-    }
-    return result
-}
-
 let getCoins = async (headersList) => {
     let result = []
     const response = await fetch(`${baseEndPoint}/getCoinMaster`, {
@@ -35,7 +13,28 @@ let getCoins = async (headersList) => {
     }).then((res) => res.json())
 
     for (const item of response) {
-        result.push(((item.coin_name).replace("usdt", "/usdt")).toUpperCase())
+        // Đổi tên coin sang cấu trúc của ccxt
+        result[item.coin_name.replace("usdt", "/usdt").toUpperCase()] = item.decimal_places
+    }
+    return result
+}
+
+let getPrice = async (coins) => {
+    let result = []
+
+    if (coins == undefined || Object.keys(coins).length == 0) return result
+
+    const exchange = new ccxt.huobi();
+    const response = await exchange.fetchTickers(Object.keys(coins))
+    for (const symbol in response) {
+        const item = {
+            coinName: symbol,
+            bid: response[symbol].bid,
+            ask: response[symbol].ask,
+            average: ((response[symbol].ask + response[symbol].bid) / 2).toFixed(coins[symbol]),
+            spread: (response[symbol].bid && response[symbol].ask) ? response[symbol].ask - response[symbol].bid : undefined
+        }
+        result.push(item)
     }
     return result
 }
@@ -67,7 +66,7 @@ let main = async () => {
         const prices = await getPrice(coins)
         const result = await updateCurrentPrice(prices, headersList)
         console.log(result);
-    }, 2000);
+    }, 3000);
 }
 
 main();
